b21fe1ef3dc981e29af223a166a1b4fe006ac8eb
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-27 18:55:31 -0800

    deploy

---
+++ build/precache-manifest.b95b12fd13ced98a3566c6dc25eb7608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e60dda26a10a95b3da8f",
+    "url": "/static/js/main.e60dda26.chunk.js"
+  },
+  {
+    "revision": "a8f628c2b0663011ebb5",
+    "url": "/static/js/2.a8f628c2.chunk.js"
+  },
+  {
+    "revision": "e60dda26a10a95b3da8f",
+    "url": "/static/css/main.aafbc53e.chunk.css"
+  },
+  {
+    "revision": "055d99994c6e015911d3bdbc8ead3ff2",
+    "url": "/index.html"
+  }
+];
